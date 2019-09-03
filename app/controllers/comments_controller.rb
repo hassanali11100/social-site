d494669f787c4ec35e654775e870044452c7f2b2
hassanali11100 <hassanali11100@gmail.com>
2019-09-03 11:52:44 +0500

    like funcitonality added on comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     before_action :set_post
+    before_action :set_comment, only: [:like]
     
     def new
         @comment = Comment.new
@@ -15,10 +16,19 @@ class CommentsController < ApplicationController
         end
     end
 
+    def like
+        @comment.increment!(:like_count)
+        redirect_to @post
+    end
+
     private
 
     def set_post
         @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
     end
 
     def comment_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_061128) do
+ActiveRecord::Schema.define(version: 2019_09_03_062415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_061128) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
+    t.integer "like_count"
   end
 
   create_table "posts", force: :cascade do |t|
