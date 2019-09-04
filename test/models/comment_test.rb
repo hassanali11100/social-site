000e63f2ab7ffce2efeb9da4889c775baa8914f5
hassanali11100 <hassanali11100@gmail.com>
2019-09-04 11:56:37 +0500

    model comment - tests added

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+    @comment = comments(:one)
+  end
+  
+  test "should save a valid comment" do
+    @validComment = Comment.new({content: @comment.content, username: @comment.username, post_id: @post.id})
+    assert @validComment.save
+  end
+
+  test "should not save an invalid comment" do
+    @invalidComment = Comment.new({username: @comment.username, post_id: @post.id})
+    assert_not @invalidComment.save
+  end
 end
