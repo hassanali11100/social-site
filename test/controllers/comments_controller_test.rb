60469d97a711a7c6907354eac563149c9a964410
hassanali11100 <hassanali11100@gmail.com>
2019-09-04 11:15:22 +0500

    test comment - tests added for controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+    @comment = comments(:one)
+  end
+
+  test "should get new" do
+    get new_post_comment_url(@post)
+    assert_response :success
+  end
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post post_comments_url(@post), params: { comment: { content: @comment.content, username: @comment.username } }
+    end
+  end
+
+  test "should like comment" do
+    assert_difference('Comment.find(@comment.id).like_count', 1) do
+      patch like_post_comment_url(@post, @comment)
+    end
+  end
 end
